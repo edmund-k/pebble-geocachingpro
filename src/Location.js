f2fe5c8a410b51f6cc4578a4e6cd782085431c58
edmund-k <github@edmundk.de>
2015-08-21 23:02:39 +0200

    Integrated offline config page for setting new loc

--- src/Location.js
+++ src/Location.js
@@ -33,6 +33,37 @@ var proximityAlert, proximityAlertDistance;  // Distance within which to fire th
 var plusorminussymbol = "\u00B1";
 var hintMessage = "";
 var locationsNeedSending = false;
+var uri = "http://x.setpebble.com/" + setPebbleToken + "/" + Pebble.getAccountToken();
+
+/* Create Base64 Object, 
+ * Code courtesy of Nicholas Cerminara, Twitter: @nickforthough, 
+ * Source: https://gist.github.com/ncerminara/11257943
+ * Code below with some slight corrections to get it working. 
+ */
+var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64;}else if(isNaN(i)){a=64;}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a);}return t;},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r);}if(a!=64){t=t+String.fromCharCode(i);}}t=Base64._utf8_decode(t);return t;},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128);}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128);}}return t;},_utf8_decode:function(e){var t="";var n,r,c1,c2;n=r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++;}else if(r>191&&r<224){c1=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c1&63);n+=2;}else{c1=e.charCodeAt(n+1);c2=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c1&63)<<6|c2&63);n+=3;}}return t;}};
+
+/* Next: Build a simple configuration page with one text input field and a cancel / submit option.
+ * Display that text in the watch app on the Pebble. 
+ */
+var configHtml;
+configHtml  = "<html><head></head><body>";
+configHtml += "<p>Hi, I'm an offline Pebble configuration page!</p>";
+configHtml += "<p><textarea cols='40' rows='1' id='input'></textarea></p>";
+configHtml += "<p><button id='save_button'>Set as new location</button></p>";
+configHtml += "<p><a href='" + uri + "'>Go to online configuration</button></p>";
+configHtml += "<script>";
+configHtml += "function saveOptions() {";
+  configHtml += "return {";
+    configHtml += "input : document.getElementById(\"input\").value";
+  configHtml += "};";
+configHtml += "};";
+configHtml += "document.getElementById(\"save_button\").addEventListener(\"click\", function() {";
+  configHtml += "document.location = \"pebblejs://close#\" + encodeURIComponent(JSON.stringify(saveOptions()));";
+configHtml += "}, false);";
+configHtml += "</script>";
+//configHtml += "<p><a href='javascript:document.location=\"pebblejs://close#\" + encodeURIComponent(JSON.stringify(saveOptions()))'>Working save options</a></p>";
+//configHtml += "<p><a href='javascript:document.location=\"pebblejs://close#\" + encodeURIComponent(JSON.stringify({input:document.getElementById(\"input\").value}))'>Working a href</a></p>";
+configHtml += "</body></html>";
 
 Pebble.addEventListener("ready", function(e) {
   refLat = parseFloat(localStorage.getItem("lat2")) || null;
@@ -60,6 +91,27 @@ Pebble.addEventListener("ready", function(e) {
   console.log("JavaScript app ready and running! " + e.type, e.ready, "refLat="+refLat, " refLong="+refLong, " interval="+interval, " runtime="+runtime, " imperial="+imperial);
 });
 
+function setReference(reference) {
+    var colonIndex = reference.indexOf(": ");
+    if (colonIndex > 0)
+      reference = reference.slice(colonIndex+1,reference.length);
+    reference = reference.trim();
+    var spaceIndex = reference.indexOf(" ");
+    if (spaceIndex > 0) {
+      refLat = parseFloat(reference.slice(0,spaceIndex));
+      refLong = parseFloat(reference.slice(spaceIndex+1,reference.length));
+      if (reference[spaceIndex-1] == 'S') refLat = -refLat;
+      if (reference[reference.length-1] == 'W') refLong = -refLong;
+      localStorage.setItem("lat2", refLat);
+      localStorage.setItem("lon2", refLong);
+    } else {
+      settingReference = true;
+    }
+    console.log("Reference string = " + reference, "refLat = " + refLat, "refLong = "+refLong);
+    startGettingLocation();
+    return 0;
+}    
+
 Pebble.addEventListener("appmessage",
   function(e) {
     if (e && e.payload && e.payload.cmd) {
@@ -74,24 +126,8 @@ Pebble.addEventListener("appmessage",
           locationNumber = Number((e.payload.cmd).substring(3));
           var reference = locations[locationNumber];
           console.log("Reference string = " + reference);
-          var colonIndex = reference.indexOf(": ");
-          if (colonIndex > 0)
-            reference = reference.slice(colonIndex+1,reference.length);
-          reference = reference.trim();
-          var spaceIndex = reference.indexOf(" ");
-          if (spaceIndex > 0) {
-            refLat = parseFloat(reference.slice(0,spaceIndex));
-            refLong = parseFloat(reference.slice(spaceIndex+1,reference.length));
-            if (reference[spaceIndex-1] == 'S') refLat = -refLat;
-            if (reference[reference.length-1] == 'W') refLong = -refLong;
-            localStorage.setItem("lat2", refLat);
-            localStorage.setItem("lon2", refLong);
-          } else {
-            settingReference = true;
-          }
-          console.log("Reference string = " + reference, "refLat = " + refLat, "refLong = "+refLong);
-          startGettingLocation();
-          break;
+          setReference(reference);
+        break;
         case 'get':    // Restart the acquisition process.
           startGettingLocation();
           break;
@@ -128,15 +164,10 @@ Pebble.addEventListener("appmessage",
 
 Pebble.addEventListener("showConfiguration",
   function() {
-    var uri = "http://x.setpebble.com/" + setPebbleToken + "/" + Pebble.getAccountToken();
-    console.log("### Configuration url: " + uri);
-    var htmlData = "<html><head></head><body>Onboard text<br><a href='" + uri + "'>Offboard settings page</a><br></body></html>"
-    var encodedData = encode64(htmlData);
-    uri = "data:text/html;charset=utf-8;base64," + encodedData;
-    console.log("### Configuration url: " + uri);
+    console.log("Show configuration");
+    //var uri = "http://x.setpebble.com/" + setPebbleToken + "/" + Pebble.getAccountToken();
+    var uri = 'data:text/html;charset=utf-8;base64,' + Base64.encode(configHtml);
     Pebble.openURL(uri);
-    //var decodedData = decode64(encodedData);
-    //console.log("### Decoded data: " + decodedData);
   }
 );
 
@@ -144,23 +175,34 @@ Pebble.addEventListener("webviewclosed",
   function(e) {
     console.log("Webview window returned: " + e.response);
     var options = JSON.parse(decodeURIComponent(e.response));
-    imperial = (options["1"] === 1);
-    console.log("Units set to: " + (imperial ? "imperial" : "metric"));
-    localStorage.setItem("imperial", (imperial ? 1 : 0));
-    interval = parseInt(options["2"] || 10);
-    console.log("Interval set to: " + interval);
-    localStorage.setItem("interval", interval);
-    runtime = parseInt(options["3"] || 5);
-    console.log("RunTime set to: " + runtime);
-    localStorage.setItem("runtime", runtime);
-    proximityAlertDistance = parseInt(options["4"] || 0);
-    console.log("Proximity Alert set to: " + proximityAlertDistance);
-    localStorage.setItem("proximity", proximityAlertDistance);
-    for	(locationNumber = 0; locationNumber < numberOfLocations; locationNumber++) {
-      locations[locationNumber] = options[9+locationNumber];
-      localStorage.setItem("location"+locationNumber, locations[locationNumber]);
+    if(options.input)  {
+      console.log("Response input: " + options.input);
+      /*locations[0] = options.input;
+      setReference(locations[0]);
+      Pebble.showSimpleNotificationOnPebble("Set location to: ", locations[0]);*/
+      setReference(options.input);
+      Pebble.showSimpleNotificationOnPebble("Geocaching Pro: ", options.input);
     }
-    populateMenu();   
+    else {      
+      console.log("Online configuration: " + options);
+      imperial = (options["1"] === 1);
+      console.log("Units set to: " + (imperial ? "imperial" : "metric"));
+      localStorage.setItem("imperial", (imperial ? 1 : 0));
+      interval = parseInt(options["2"] || 10);
+      console.log("Interval set to: " + interval);
+      localStorage.setItem("interval", interval);
+      runtime = parseInt(options["3"] || 5);
+      console.log("RunTime set to: " + runtime);
+      localStorage.setItem("runtime", runtime);
+      proximityAlertDistance = parseInt(options["4"] || 0);
+      console.log("Proximity Alert set to: " + proximityAlertDistance);
+      localStorage.setItem("proximity", proximityAlertDistance);
+      for	(locationNumber = 0; locationNumber < numberOfLocations; locationNumber++) {
+        locations[locationNumber] = options[9+locationNumber];
+        localStorage.setItem("location"+locationNumber, locations[locationNumber]);
+      }
+      populateMenu();   
+    }
     if (runtime > 0) {
       var msg = {
         "location0": locations[0], "location1": locations[1], "location2": locations[2], "location3": locations[3], "location4": locations[4],
@@ -486,91 +528,3 @@ function deleteUserPin(pin, callback) {
 }
 
 /***************************** end timeline lib *******************************/
-
-/***************************** base64 encode decode lib *******************************/
-
-var keyStr = "ABCDEFGHIJKLMNOP" +
-             "QRSTUVWXYZabcdef" +
-             "ghijklmnopqrstuv" +
-             "wxyz0123456789+/" +
-             "=";
-
-function encode64(input) {
-     //input = escape(input);
-     var output = "";
-     var chr1, chr2, chr3 = "";
-     var enc1, enc2, enc3, enc4 = "";
-     var i = 0;
-
-     do {
-        chr1 = input.charCodeAt(i++);
-        chr2 = input.charCodeAt(i++);
-        chr3 = input.charCodeAt(i++);
-
-        enc1 = chr1 >> 2;
-        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-        enc4 = chr3 & 63;
-
-        if (isNaN(chr2)) {
-           enc3 = enc4 = 64;
-        } else if (isNaN(chr3)) {
-           enc4 = 64;
-        }
-
-        output = output +
-           keyStr.charAt(enc1) +
-           keyStr.charAt(enc2) +
-           keyStr.charAt(enc3) +
-           keyStr.charAt(enc4);
-        chr1 = chr2 = chr3 = "";
-        enc1 = enc2 = enc3 = enc4 = "";
-     } while (i < input.length);
-
-     return output;
-  }
-
-function decode64(input) {
-     var output = "";
-     var chr1, chr2, chr3 = "";
-     var enc1, enc2, enc3, enc4 = "";
-     var i = 0;
-
-     // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-     /*var base64test = /[^A-Za-z0-9\+\/\=]/g;
-     if (base64test.exec(input)) {
-        alert("There were invalid base64 characters in the input text.\n" +
-              "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
-              "Expect errors in decoding.");
-     }*/
-     input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-
-     do {
-        enc1 = keyStr.indexOf(input.charAt(i++));
-        enc2 = keyStr.indexOf(input.charAt(i++));
-        enc3 = keyStr.indexOf(input.charAt(i++));
-        enc4 = keyStr.indexOf(input.charAt(i++));
-
-        chr1 = (enc1 << 2) | (enc2 >> 4);
-        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-        chr3 = ((enc3 & 3) << 6) | enc4;
-
-        output = output + String.fromCharCode(chr1);
-
-        if (enc3 != 64) {
-           output = output + String.fromCharCode(chr2);
-        }
-        if (enc4 != 64) {
-           output = output + String.fromCharCode(chr3);
-        }
-
-        chr1 = chr2 = chr3 = "";
-        enc1 = enc2 = enc3 = enc4 = "";
-
-     } while (i < input.length);
-
-     //return unescape(output);
-     return output;
-  }
-
-/***************************** end base64 encode decode lib *******************************/
